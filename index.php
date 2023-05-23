7ed78639f7c26af02a7e13ded37257c9ac826597
Ed <seibeledouard@yahoo.fr>
2023-05-23 23:26:11 +0200

    tentative de correction d'erreur

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+/*require_once __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$dotenv->load();*/
 
 //require_once 'databaseCreation.php';
 //require_once 'databaseInsert.php';
